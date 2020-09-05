0714d61952dbe192f0a8ad9d61ffeb92b28b62fd
sulove <fluttermaniac.np@gmail.com>
2020-09-05 18:36:02 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/sulove/flutter_checkboxes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sulove.flutter_checkboxes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Checkboxes Demo',
+      debugShowCheckedModeBanner: false,
+      home: CheckboxesDemo(),
+     // theme: _buildShrineTheme(),
+    );
+  }
+}
+
+class CheckboxesDemo extends StatefulWidget {
+  @override
+  _CheckboxesDemoState createState() => _CheckboxesDemoState();
+}
+
+class _CheckboxesDemoState extends State<CheckboxesDemo> {
+  List<bool> checked = [true, true, false, false, true];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Checkboxes Demo'),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 40),
+        child: Column(
+          children: [
+            for (var i = 0; i < 5; i += 1)
+              Row(
+                children: [
+                  Checkbox(
+                    onChanged: i == 4
+                        ? null
+                        : (bool value) {
+                      setState(() {
+                        checked[i] = value;
+                      });
+                    },
+                    tristate: i == 1,
+                    value: checked[i],
+                  ),
+                  Text(
+                    'Checkbox ${i + 1}',
+                    style: Theme.of(context).textTheme.subtitle1.copyWith(color: i == 4 ? Colors.black38 : Colors.black),
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
